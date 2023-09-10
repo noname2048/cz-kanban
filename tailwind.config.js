9b4226d69c24674beb3c3a7e27148fae5229c661
Sungwook Choi <sungwook.csw@gmail.com>
2023-09-11 00:55:00 +0900

    chore: Set mainBg, columnBg

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: "#0D1117",
+        column: "#161C22",
+      },
+    },
   },
   plugins: [],
-}
+};
